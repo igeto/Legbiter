cda178d3ba0588431b225e85ce27af70f520fb00
Code <code@SEDC.local>
2017-09-15 19:46:39 +0200

    Integrated ng-admin.

--- Legbiter.Web/client/src/app/app.component.ts
+++ Legbiter.Web/client/src/app/app.component.ts
@@ -1,16 +1,54 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http'
+import { Component, ViewContainerRef } from '@angular/core';
+import * as $ from 'jquery';
+
+import { GlobalState } from './global.state';
+import { BaImageLoaderService, BaThemePreloader, BaThemeSpinner } from './theme/services';
+import { BaThemeConfig } from './theme/theme.config';
+import { layoutPaths } from './theme/theme.constants';
+
+/*
+ * App Component
+ * Top Level Component
+ */
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app',
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <main [class.menu-collapsed]="isMenuCollapsed" baThemeRun>
+      <div class="additional-bg"></div>
+      <router-outlet></router-outlet>
+    </main>
+  `
 })
-export class AppComponent implements OnInit {
-	constructor(private _httpService: Http) { }
-	apiValues: string[] = [];
-	ngOnInit() {
-		this._httpService.get('/api/values').subscribe(values => {
-			this.apiValues = values.json() as string[];
-		});
-	}
+export class App {
+
+  isMenuCollapsed: boolean = false;
+
+  constructor(private _state: GlobalState,
+              private _imageLoader: BaImageLoaderService,
+              private _spinner: BaThemeSpinner,
+              private viewContainerRef: ViewContainerRef,
+              private themeConfig: BaThemeConfig) {
+
+    themeConfig.config();
+
+    this._loadImages();
+
+    this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
+      this.isMenuCollapsed = isCollapsed;
+    });
+  }
+
+  public ngAfterViewInit(): void {
+    // hide spinner once all loaders are completed
+    BaThemePreloader.load().then((values) => {
+      this._spinner.hide();
+    });
+  }
+
+  private _loadImages(): void {
+    // register some loaders
+    BaThemePreloader.registerLoader(this._imageLoader.load('assets/img/sky-bg.jpg'));
+  }
+
 }
